8bf9a00bbd6c548fa06f5f0b9132d22a83e0167b
Yatrik <yatrik.golakia.weenggs@gmail.com>
2025-12-01 14:22:10 +0530

    Demo has been change !

--- Demo.jsx
+++ Demo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Demo(){
     return(
         <>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque soluta magni nobis magnam placeat odio, accusamus laudantium aliquid provident. Tempora doloribus possimus quos mollitia eligendi, adipisci obcaecati fugiat quidem inventore ipsum quasi quam id cum!</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est deleniti eius, ut consequuntur odio laborum libero enim natus vitae, veniam tempora quasi eaque minus inventore porro consectetur quidem dolores sint nulla laudantium, aspernatur eligendi hic mollitia ab! Voluptate sit voluptas modi exercitationem quasi harum, magnam fugit quod ipsum dolorum vitae enim aut atque minima iste consequatur sunt obcaecati natus. Error eligendi accusantium quae voluptatibus impedit at obcaecati minus, accusamus, saepe vitae aperiam officiis, odit facilis.</p>
         </>
     );
 }
