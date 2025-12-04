eaf24e75006350df5af5001d7131c3df42f29787
yatrikgolakiaweenggs-dev <yatrik.golakia.weenggs@gmail.com>
2025-12-04 11:33:50 +0530

    Add files via upload

---
+++ Demo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Demo(){
+    return(
+        <>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, exercitationem nemo? Sapiente sequi illum delectus maxime earum iure, autem et veritatis veniam sint! Pariatur reiciendis temporibus rerum modi. Sed magnam et sit labore iste fuga earum quae, eos eligendi aut ducimus deleniti maxime accusamus ex, repellendus dolores error velit sint.</p>
+        </>
+    );
+}
